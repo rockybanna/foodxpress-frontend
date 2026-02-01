7fa16babcc2a84100f0380bdf099bb59b3d50834
rockybanna <ajayrajpoot420@gmail.com>
2026-02-01 15:23:20 +0530

    Set backend Apps Script URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Global configuration (read-only)
 window.CONFIG = {
-  BACKEND_URL: "PASTE_APPS_SCRIPT_URL_LATER"
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzcXj-R0fgjPVsUy9nQApv5SOxuscqZ_EK1-F6ag6dXL-rrDEkx7qHyylvlhwXDBKNu/exec"
 };
