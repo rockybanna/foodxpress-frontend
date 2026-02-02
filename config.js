519b399fbdf62c34bd3bc6e5b970a915f6b9a989
rockybanna <ajayrajpoot420@gmail.com>
2026-02-02 13:03:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Global configuration (locked)
 window.CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbw0KSdAB050IK7xTLdHco8QS-elkHB6sbDvT8HvDQJVELe8OyYEy7HVaMSujAE7Om-N/exec"
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyRKTJv3mj-c-h-MPbCiPpwIDcrAcOrtOzHErUOYcJpJ9d-jmfVPFOY-79xJgCNPYVz/exec"
 };
